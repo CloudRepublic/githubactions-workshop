3a92b4902dc8f4b85c650ec1265f1c6c407aafb5
Marco Ippel <marco@ippelsoft.nl>
2022-07-10 10:30:35 +0200

    Function with unittest added

---
+++ src/HelloWorld.UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace HelloWorld.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test_ShouldPass()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test_ShouldFail()
+        {
+            Assert.True(false);
+        }
+    }
+}
